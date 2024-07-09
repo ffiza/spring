17f1af8624a093592faeccf450733c201e41f64a
Federico Iza <fedegiza@gmail.com>
2024-07-09 19:07:08 -0300

    Update `setup.py`.

--- setup.py
+++ setup.py
@@ -5,4 +5,4 @@ from setuptools import find_packages
 setup(name="spring",
       version="1.0.0",
       author="Federico Iza",
-      packages=["examples", "spring"])
+      packages=find_packages())
